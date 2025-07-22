a098a94be7f03317c63ee643745636d3915e4e57
levonk <v3l8dud3@lkara.com>
2025-07-21 17:12:06 -0700

    clean up the repo

--- home/dot_config/shells/shared/network-aliases.sh
+++
@@ -1,5 +0,0 @@
-# This file is managed by chezmoi (https://www.chezmoi.io/) and maintained at https://github.com/levonk/dotfiles
-# Network and utility aliases (from legacy sharedrc)
-
-# Get the remote IP address
-alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
