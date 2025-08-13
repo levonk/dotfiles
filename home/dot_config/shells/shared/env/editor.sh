5057c457d659c40dc45286033e4eda3617a07ecf
levonk <v3l8dud3@lkara.com>
2025-08-12 23:57:03 -0700

    refactor(env): Reorganize editor and word char config
    Move WORDCHARS from shared-env.cfg to editor.sh for better organization:
    - Add comprehensive editor environment variables (VISUAL, GIT_EDITOR)
    - Move WORDCHARS to editor.sh as it's line editor related
    - Add descriptive comments for all editor variables
    - Improve logical grouping of shell configuration settings
    
    This reorganization makes the configuration more maintainable
    and groups related settings together logically.

--- home/dot_config/shells/shared/env/editor.sh
+++ home/dot_config/shells/shared/env/editor.sh
@@ -2,5 +2,13 @@
 #------------------------------------------------------------------------------
 # Editor Configuration
 #------------------------------------------------------------------------------
-export EDITOR=nvim
-export CVSEDITOR=nvim
+export EDITOR=nvim         # Default editor for CLI tools
+export VISUAL=nvim         # Used by some GUI wrappers or fallback editors
+export CVSEDITOR=nvim      # Used by CVS version control
+export GIT_EDITOR=nvim     # Git commit/edit operations
+
+#------------------------------------------------------------------------------
+# Word Characters for Line Editor
+#------------------------------------------------------------------------------
+export WORDCHARS='*?_-.[]~=&;!#$%^(){}<>;'
+
