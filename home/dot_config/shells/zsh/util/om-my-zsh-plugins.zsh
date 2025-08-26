be0c88911311a8658624947945b119ed8d2fce74
levonk <277861+levonk@users.noreply.github.com>
2025-08-26 10:47:58 -0700

    chore(zsh): fix p10k clone destination
    Clone Powerlevel10k into the plugins directory without extra nesting so powerlevel10k.zsh resolves at the expected path under XDG shells/zsh.

--- home/dot_config/shells/zsh/util/om-my-zsh-plugins.zsh
+++ home/dot_config/shells/zsh/util/om-my-zsh-plugins.zsh
@@ -3,13 +3,13 @@
 #------------------------------------------------------------------------------
 # OhMyZsh Plugin Manager
 #------------------------------------------------------------------------------
-export ZSH="${XDG_CONFIG_HOME:-$HOME/.config}/zsh/oh-my-zsh.zsh"
+export ZSH="${XDG_CONFIG_HOME:-$HOME/.config}/shells/zsh/oh-my-zsh.zsh"
 ZSH_THEME="robbyrussell"
 plugins=(git)
 source $ZSH/oh-my-zsh.sh
 
-if [[ ! -f ${XDG_CONFIG_HOME:-$HOME/.config}/zsh/prompts/prompt.zsh ]]; then
+if [[ ! -f ${XDG_CONFIG_HOME:-$HOME/.config}/shells/zsh/prompts/prompt.zsh ]]; then
   echo "Installing Powerlevel10k..."
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${XDG_CONFIG_HOME:-$HOME/.config}/zsh/plugins/powerlevel10k/powerlevel10k
-  echo 'source ${XDG_CONFIG_HOME:-$HOME/.config}/zsh/plugins/powerlevel10k/powerlevel10k.zsh' >>! ${XDG_CONFIG_HOME:-$HOME/.config}/zsh/prompts/prompt.zsh
+  git clone --depth=1 https://github.com/levonk/powerlevel10k.git ${XDG_CONFIG_HOME:-$HOME/.config}/shells/zsh/plugins/powerlevel10k
+  echo 'source ${XDG_CONFIG_HOME:-$HOME/.config}/shells/zsh/plugins/powerlevel10k/powerlevel10k.zsh' >>! ${XDG_CONFIG_HOME:-$HOME/.config}/shells/zsh/prompts/prompt.zsh
 fi
