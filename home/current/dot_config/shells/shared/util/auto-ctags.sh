90207187add4517edf92e4fd0a9e1d274992021d
levonk <277861+levonk@users.noreply.github.com>
2025-09-03 11:13:03 -0700

    feat(auto-ctags): add safe auto-indexing utility with /home/micro guard to prevent long scans

---
+++ home/current/dot_config/shells/shared/util/auto-ctags.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## ctags indexes all the identifiers in source code for rapid navigation
+## The following files make it so entry into a directory automatically
+## initializes index
+
+## ~/.zshrc or ~/.bashrc
+function auto_ctags() {
+  # Skip running in the home directory to avoid indexing $HOME
+  if [ "$PWD" = "$HOME" ] || [ "$PWD/" = "$HOME/" ]; then
+    return 0
+  fi
+  if [ -f ".ctags" ] || [ -d "src" ]; then
+    echo "Indexing with ctags..."
+    ctags -R .
+  fi
+}
+
+## Under Zsh defined in `$XDG_CONFIG_HOME/shells/zsh/util/auto-ctags.zsh`
+# `autoload -U add-zsh-hook`
+# `add-zsh-hook chpwd auto_ctags`
+
+## Under Bash defined in `$XDG_CONFIG_HOME/shells/bash/util/auto-ctags.bash`
+# `export PROMPT_COMMAND="auto_ctags; $PROMPT_COMMAND"`
