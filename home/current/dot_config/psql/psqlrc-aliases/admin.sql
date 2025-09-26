c0d6e562e6e9d40b8454850886a18547698d9309
levonk <277861+levonk@users.noreply.github.com>
2025-09-26 09:43:04 -0700

    fix(psql): correct invalid include path in psqlrc
    The psqlrc.conf template was trying to include a non-existent file,
    `psqlrc-admin`, which caused test failures. This change introduces an
    aggregator file, `admin.sql`, to include all admin aliases and updates
    the `psqlrc.conf` to point to the correct file.

---
+++ home/current/dot_config/psql/psqlrc-aliases/admin.sql
@@ -0,0 +1,14 @@
+-- Include all admin aliases
+\ir ~/.config/psql/psqlrc-aliases/admin/activity.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/autovac_runtime.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/dbsize.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/enums.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/largest_objects.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/last_vacuum_analyze.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/locks.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/rtsize.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/tablesize.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/tsize.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/uselesscol.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/vac_ready.sql
+\ir ~/.config/psql/psqlrc-aliases/admin/waits.sql
