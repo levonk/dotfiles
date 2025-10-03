053d9a62e811adf0392cc2d0fb44c4b27b7d7a17
levonk <277861+levonk@users.noreply.github.com>
2025-10-02 23:10:07 -0700

    startup: Debug messages to see loading, particularly mise

--- home/current/dot_config/shells/shared/env/mise-env.sh
+++ home/current/dot_config/shells/shared/env/mise-env.sh
@@ -7,8 +7,10 @@ _MISE_SHIMS="$HOME/.local/share/mise/shims"
 
 case ":${PATH}:" in
     *:"$_MISE_SHIMS":*)
+		echo "Debug: Mise shims already in PATH" >&2
         ;;
     *)
+		echo "Debug: Prepending Mise shims to PATH" >&2
         # Prepending path in case a system-installed binary needs to be overridden
         export PATH="$_MISE_SHIMS:$PATH"
         ;;
