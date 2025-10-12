104d5fca8219b4068777937716c6f4d949b7d981
levonk <277861+levonk@users.noreply.github.com>
2025-10-12 01:30:28 -0700

    tests(startup): properly identify user

--- scripts/tests/run-ci-tests.sh
+++ scripts/tests/run-ci-tests.sh
@@ -127,7 +127,7 @@ run_chezmoi_test_for_user() {
     local script_output
     local script_status=0
     echo "[debug] Executing login shell for user '$user' to collect environment..."
-    local command_to_run='printf "__STARTUP_VARS__BUN_INSTALL=%s|PATH=%s|USER=%s|SHELL=%s|STARTUP_TEST_ENV=%s\n" "${BUN_INSTALL-}" "${PATH-}" "$USER" "$SHELL" "${STARTUP_TEST_ENV-}"'
+    local command_to_run='printf "__STARTUP_VARS__BUN_INSTALL=%s|PATH=%s|USER=%s|SHELL=%s|STARTUP_TEST_ENV=%s\n" "${BUN_INSTALL-}" "${PATH-}" "$(id -un)" "$SHELL" "${STARTUP_TEST_ENV-}"'
 
     if ! script_output="$(sudo -E -H -u "$user" "$shell" -li -c "$command_to_run" 2>&1)"; then
         script_status=$?
