38373e3c87332c3e3abae58e10657330c5e534a5
levonk <277861+levonk@users.noreply.github.com>
2025-10-12 18:59:27 -0700

    startup: don't use sourceable to prevent trap

--- home/current/dot_config/shells/shared/entrypointrc.sh
+++ home/current/dot_config/shells/shared/entrypointrc.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 # shellcheck shell=sh
-#{{- includeTemplate "dot_config/ai/templates/shell/sourceable.sh.tmpl" (dict "path" .path "name" .name) -}}
 
 # =============================================================================
 # Optimized Shell Entry Point RC (Modular)
